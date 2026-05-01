a0d9e42f1394c30473b8031f5e70121212b1955f
TASNEEM BANU <sh29tasneem@gmail.com>
2026-05-01 21:42:04 +0530

    feat: add project documentation, update ignore rules, and modify home route response message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return "Docker CI/CD with Python Flask 🚀"
+    return "Docker CI/CD with Python Flask 🚀. Working successful!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
